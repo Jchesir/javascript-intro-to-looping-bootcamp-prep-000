67e389aa9d6de6dbdf612788e026cb87aabc79f9
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-03-15 19:14:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,6 +24,9 @@ function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
     while (maybeTrue())
     return array
 }
